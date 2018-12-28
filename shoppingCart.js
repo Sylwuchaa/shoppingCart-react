1c0bdfbc34e36d14f566e3c1ff046cf7d2289bcd
Sylwcuhaa <uzumagi1@gmail.com>
2018-12-28 17:38:26 +0100

    added buy button and next handlers buttons

--- shoppingCart.js
+++ shoppingCart.js
@@ -3,20 +3,31 @@ class ShoppingCart extends React.Component {
     availableProducts: 7,
     shoppingCart: 3
   }
-  removeFromCart = () => {
+  handleRemoveFromCart = () => {
     this.setState ({
       shoppingCart: this.state.shoppingCart -1
+    })
+  }
+  handleAddToCart = () => {
+    this.setState({
+      shoppingCart: this.state.shoppingCart +1
+    })
+  }
+  handleBuyButton = () => {
+    this.setState({
+      shoppingCart: 0
     })
   }
   render() {
     return (
       <div>
-        <button disabled={this.state.shoppingCart === 0? true : false} onClick={this.removeFromCart}> - </button>
+        <button disabled={this.state.shoppingCart === 0? true : false} onClick={this.handleRemoveFromCart}> - </button>
         <span> {this.state.shoppingCart} </span>
-        <button> + </button>
+        <button disabled={this.state.shoppingCart === this.state.availableProducts? true : false} onClick={this.handleAddToCart}> + </button>
+        {this.state.shoppingCart > 0 && <button onClick={this.handleBuyButton}>Kup</button>}
       </div>
     )
-  }
+ }
 }
 
 ReactDOM.render(<ShoppingCart />, document.getElementById('root'))
