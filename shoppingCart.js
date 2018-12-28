2a354df1eb0e56e96669a1167b0b278e4b3d9a42
Sylwcuhaa <uzumagi1@gmail.com>
2018-12-28 17:09:11 +0100

    added some simple style and handle from the remove button

--- shoppingCart.js
+++ shoppingCart.js
@@ -3,10 +3,15 @@ class ShoppingCart extends React.Component {
     availableProducts: 7,
     shoppingCart: 3
   }
+  removeFromCart = () => {
+    this.setState ({
+      shoppingCart: this.state.shoppingCart -1
+    })
+  }
   render() {
     return (
       <div>
-        <button> - </button>
+        <button disabled={this.state.shoppingCart === 0? true : false} onClick={this.removeFromCart}> - </button>
         <span> {this.state.shoppingCart} </span>
         <button> + </button>
       </div>
